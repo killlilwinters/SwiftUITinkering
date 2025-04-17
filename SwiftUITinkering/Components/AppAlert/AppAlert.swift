daf32715729487e338fbe3f1fce7e9f022adf73a
MaksGorobets <gorobets542@gmail.com>
2025-04-17 16:13:58 +0300

    Some cleanup and improvement regarding the AppAlert

--- SwiftUITinkering/Components/AppAlert/AppAlert.swift
+++ SwiftUITinkering/Components/AppAlert/AppAlert.swift
@@ -7,8 +7,10 @@
 
 import SwiftUI
 
-enum AppAlertRole {
-    case success, warning, failure
+enum AppAlertRole: String, CaseIterable {
+    case success
+    case warning
+    case failure
     
     var color: Color {
         switch self {
@@ -32,6 +34,7 @@ struct AppAlert: ViewModifier {
     let role: AppAlertRole
     let title: String
     let subTitle: String?
+    var cornerRadius: CGFloat = 20
     
     let disappearDelay: Int = 4
     
@@ -40,31 +43,30 @@ struct AppAlert: ViewModifier {
     
     func body(content: Content) -> some View {
         content
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .allowsHitTesting(!keep)
             .blur(radius: keep ? 5 : 0)
             .overlay {
                 if isPresented {
                     VStack {
-                        AppAlertView(role: role, title: title, subTitle: subTitle, keep: $keep)
+                        AppAlertView(role: role, title: title, subTitle: subTitle, keep: $keep, isPresented: $isPresented)
                         Spacer()
                     }
-                    .transition(. move(edge: .top))
+                    .transition(.move(edge: .top))
                     .animation(.easeInOut, value: isPresented)
-                    .onAppear {
-                        Task {
-                            try await Task.sleep(for: .seconds(disappearDelay))
-                            guard !keep else { return }
+                    .task(id: keep) {
+                        try? await Task.sleep(for: .seconds(disappearDelay))
+                        
+                        if isPresented && !keep {
                             withAnimation {
                                 isPresented = false
+                                keep = false
                             }
                         }
                     }
-                    .onChange(of: keep) {
-                        Task {
-                            try await Task.sleep(for: .seconds(disappearDelay))
-                            guard !keep else { return }
-                            withAnimation {
-                                isPresented = false
-                            }
+                    .onChange(of: isPresented) { oldValue, newValue in
+                        if !newValue {
+                            keep = false
                         }
                     }
                 }
@@ -77,49 +79,71 @@ struct AppAlertView: View {
     let role: AppAlertRole
     let title: String
     var subTitle: String?
+    var cornerRadius: CGFloat = 20
     
     @State private var isUnfolded: Bool = false
-    @State private var multiplier: CGFloat = 0
+    @State private var multiplier: CGFloat = 0 {
+        didSet { print(multiplier) }
+    }
     @Binding var keep: Bool
+    @Binding var isPresented: Bool
     
     var body: some View {
         let width: CGFloat = 320
-//        var multiplier: CGFloat = 0
-//        let height: CGFloat = isUnfolded ? 300 : 60 + multiplier
-        RoundedRectangle(cornerRadius: 10)
+        RoundedRectangle(cornerRadius: cornerRadius)
             .opacity(0.7)
             .frame(
-                maxWidth: isUnfolded ? width + 30 : width,
-                maxHeight: isUnfolded ? 300 : 60 + multiplier
+                maxWidth:  isUnfolded ? width + (multiplier + 30) : width,
+                maxHeight: isUnfolded ? (300 + multiplier)        : (60 + multiplier)
             )
             .overlay {
                 contents
             }
-            .onTapGesture {
-                keep.toggle()
-                withAnimation(.bouncy) {
-                    isUnfolded.toggle()
-                }
-            }
+//            .onTapGesture {
+                //                keep.toggle()
+                //                withAnimation(.bouncy) {
+                //                    isUnfolded.toggle()
+                //                }
+//            }
             .gesture(
                 DragGesture()
-                    .onChanged{ value in
-                        guard !isUnfolded else { return }
-                        if keep == false {
-                            keep.toggle()
+                    .onChanged { value in
+                        performOnDrag(value)
+                    }
+                    .onEnded { _ in
+                        withAnimation(.bouncy) {
+                            multiplier = 0
                         }
-                        print(multiplier)
-                        multiplier = (value.translation.height / 100)
                     }
             )
-            .onChange(of: multiplier) { oldValue, newValue in
-                if newValue > 3.2 {
-                    withAnimation(.bouncy) {
-                        isUnfolded.toggle()
-                        keep = true
-                    }
+    }
+    
+    
+    func performOnDrag(_ value: DragGesture.Value) {
+        var statement = false
+        if isUnfolded {
+            
+            statement = value.translation.height < multiplier && multiplier < -2
+            
+        } else {
+            
+            statement = value.translation.height > multiplier && multiplier > 3.2
+            
+        }
+        
+        if keep == isUnfolded {
+            if statement {
+                guard isPresented else { return }
+                withAnimation(.bouncy) {
+                    keep = !isUnfolded
+                    isUnfolded = !isUnfolded
                 }
             }
+        }
+        withAnimation(.bouncy) {
+            multiplier = (value.translation.height / 80)
+        }
+        
     }
     
     @ViewBuilder
@@ -145,7 +169,9 @@ struct AppAlertView: View {
                 )
             
             if let subTitle, isUnfolded {
+                
                 Text(subTitle)
+                    .frame(width: size.width - 20)
                     .multilineTextAlignment(.center)
                     .foregroundStyle(.white)
                     .position(
@@ -153,19 +179,19 @@ struct AppAlertView: View {
                         y: isUnfolded ? 200 : size.height / 2
                     )
                     .transition(.opacity)
+                
             }
             
         }
     }
-    
 }
 
 #Preview {
-    AppAlertView(role: .success, title: "Payment successful!", keep: .constant(false))
+    AppAlertView(role: .success, title: "Payment successful!", keep: .constant(false), isPresented: .constant(true))
 }
 
 #Preview("Landscape", traits: .landscapeRight) {
-    AppAlertView(role: .failure, title: "Payment failed.", keep: .constant(false))
+    AppAlertView(role: .failure, title: "Payment failed.", keep: .constant(false), isPresented: .constant(true))
 }
 
 #Preview("TestPreview") {
--- SwiftUITinkering/Views/ContentView.swift
+++ SwiftUITinkering/Views/ContentView.swift
@@ -10,19 +10,55 @@ import RippleAnimation
 
 struct ContentView: View {
     
-    @State private var isPresented = false
+    @State private var isSavedPresented = false
+    @State private var isAlertPresented = false
+    
+    @State private var selectedRole: AppAlertRole = .success
+    var title: String {
+        switch selectedRole {
+        case .success:
+            "Payment successful!"
+        case .warning:
+            "Something went wrong..."
+        case .failure:
+            "Insufficient funds!"
+        }
+    }
+    let subTitle = "Info for\n\(UUID())"
     
     var body: some View {
         NavigationStack {
             VStack(spacing: 20) {
 //                NavigationLink("Saved", value: "SavedView")
-                Button("Saved") { isPresented.toggle() }
+                Button("Saved") { isSavedPresented.toggle() }
                 NavigationLink("Tab bar", value: "TabBar")
                 NavigationLink("Custom shapes", value: "CustomShapes")
                 NavigationLink("Drawing", value: "DrawingView")
                 NavigationLink("Nesting list rows", value: "NestingListRows")
+                NavigationLink("Alert test", value: "AlertTestView")
+                HStack {
+                    Button("Show alert") {
+                        withAnimation {
+                            isAlertPresented.toggle()
+                        }
+                    }
+                    Picker("Pick a type", selection: $selectedRole) {
+                        ForEach(AppAlertRole.allCases, id: \.rawValue) { c in
+                            Text(c.rawValue.capitalized).tag(c)
+                        }
+                    }
+                }
             }
             .padding(.horizontal, 20)
+            .modifier(
+                AppAlert(
+                    role: selectedRole,
+                    title: title,
+                    subTitle: subTitle,
+                    cornerRadius: 20,
+                    isPresented: $isAlertPresented
+                )
+            )
             .navigationDestination(for: String.self) { string in
                 switch string {
                 case "SavedView":
@@ -35,11 +71,13 @@ struct ContentView: View {
                     DrawingView()
                 case "NestingListRows":
                     NestingListRowsView()
+                case "AlertTestView":
+                    AlertTestView()
                 default:
                     Text("Unknown destination: \(string)")
                 }
             }
-            .fullScreenCover(isPresented: $isPresented) {
+            .fullScreenCover(isPresented: $isSavedPresented) {
                 SavedView()
             }
         }
